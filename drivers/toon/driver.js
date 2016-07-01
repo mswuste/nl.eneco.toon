a53376e05e4f26a71222164d976c6c5375d644d3
Robin Bolscher <robinbolscher@gmail.com>
2016-07-01 15:39:28 +0200

    Refactoring, first try at live device

--- drivers/toon/driver.js
+++ drivers/toon/driver.js
@@ -1,8 +1,9 @@
-var Toon = require('node-toon');
-var request = require("request");
+'use strict';
 
-var devices = [];
-var toons = [];
+const Toon = require('node-toon');
+
+let devices = [];
+let temp_devices = [];
 
 /**
  * Find Toon
@@ -12,36 +13,10 @@ var toons = [];
 module.exports.init = function (devices_data, callback) {
 
 	// Loop over all installed devices and add them
-	for (var x = 0; x < devices_data.length; x++) {
-		if (devices_data[x].hasOwnProperty("id")) {
-			addDevice(devices_data[x]);
-		}
-	}
-
-	// Add device, if not already added
-	function addDevice(device_data) {
-		var toon = new Toon(Homey.env.TOON_KEY, Homey.env.TOON_SECRET);
-
-		if (Homey.manager('settings').get('toon_access_token') != null) toon.access_token = Homey.manager('settings').get('toon_access_token');
-		if (Homey.manager('settings').get('toon_refresh_token') != null) toon.refresh_token = Homey.manager('settings').get('toon_refresh_token');
-
-		toon.getAgreements(function (err, data) {
-
-			for (var i = 0; i < data.length; i++) {
-				if (data[i].agreementId == device_data.id) {
-					toon.setAgreement(data[i].agreementId);
-					var device = {
-						name: "Toon",
-						data: {
-							id: device_data.id
-						}
-					};
-					devices.push(device);
-					toons.push({id: device.data.id, toon: toon});
-				}
-			}
-		});
-	};
+	devices_data.forEach(function (device_data) {
+		module.exports.setUnavailable(device_data, __("reconnecting"));
+		initDevice(device_data);
+	});
 
 	// Start listening for changes
 	startPolling();
@@ -57,12 +32,14 @@ module.exports.init = function (devices_data, callback) {
 module.exports.pair = function (socket) {
 
 	// Create new toon instance
-	var toon = new Toon(Homey.env.TOON_KEY, Homey.env.TOON_SECRET);
+	let client = new Toon(Homey.env.TOON_KEY, Homey.env.TOON_SECRET);
+	temp_devices = [];
 
 	// Listen for the start event
 	socket.on("start", function (data, callback) {
 
-		Homey.manager('cloud').generateOAuth2Callback('https://api.toonapi.com/authorize?client_id=' + Homey.env.TOON_KEY + '&redirect_uri=https://callback.athom.com/oauth2/callback/&response_type=code',
+		// Start with fetching access tokens
+		Homey.manager('cloud').generateOAuth2Callback(`https://api.toonapi.com/authorize?response_type=code&client_id=${Homey.env.TOON_KEY}&redirect_uri=https://callback.athom.com/oauth2/callback/`,
 
 			// Before fetching authorization code
 			function (err, result) {
@@ -71,80 +48,53 @@ module.exports.pair = function (socket) {
 
 			// After fetching authorization code
 			function (err, result) {
-				toon.getAccessToken(result, "https://callback.athom.com/oauth2/callback/", function (err, result) {
-					toon.access_token = result.access_token;
-					toon.refresh_token = result.refresh_token;
 
-					Homey.manager("settings").set("toon_access_token", toon.access_token);
-					Homey.manager("settings").set("toon_refresh_token", toon.refresh_token);
+				// Get new access and refresh token
+				client.getAccessTokens(result, "https://callback.athom.com/oauth2/callback/", function (err, result) {
 
-					socket.emit("authenticated", toon.access_token);
+					// Get all devices hooked up to this account
+					client.getAgreements((err, results) => {
+						if (!err && results != null && results.length > 0) {
+
+							// Loop over agreements
+							results.forEach((data) => {
+
+								// Check if device is not added already
+								if (!getDevice(data.agreementId)) {
+
+									// Store access token in settings
+									Homey.manager("settings").set(`toon_${data.agreementId}_access_token`, result.access_token);
+									Homey.manager("settings").set(`toon_${data.agreementId}_refresh_token`, result.refresh_token);
+
+									// Store device temporarily
+									temp_devices.push({
+										name: "Toon",
+										data: {
+											id: data.agreementId
+										}
+									});
+								}
+							});
+						}
+
+						// Emit authenticated to the front-end
+						socket.emit("authenticated", result.access_token);
+					});
 				});
 			}
 		);
 	});
 
 	// Show list of devices
-	socket.on("list_devices", function (data, callback) {
-		socket.on("get_devices", function (devices) {
-			for (var i = 0; i < devices.length; i++) {
-				devices[i].name = devices[i].data.name_long
-			}
-			callback(null, devices);
-		});
+	socket.on("list_devices", (data, callback) => {
+
+		// Return results
+		callback(null, temp_devices);
 	});
 
-	// Return the found toon instances
-	socket.on("get_toon", function (data, callback) {
-
-		// Get all devices hooked up to this account
-		toon.getAgreements(function (err, data) {
-			if (!err && data != null && data.length > 0) {
-				var temp_devices = [];
-
-				for (var i = 0; i < data.length; i++) {
-					if (getDevice(data[i].agreementId) == null) {
-						temp_devices.push({
-							name: "Toon",
-							data: {
-								id: data[i].agreementId,
-								name_long: "Toon: " + data[i].street + " " + data[i].houseNumber + ", " + data[i].postalCode + " " + data[i].city.charAt(0) + data[i].city.slice(1).toLowerCase()
-							}
-						});
-					}
-				}
-				callback(null, temp_devices);
-			}
-			else {
-				callback(true, null);
-			}
-		});
-	});
-
-	// Add selected toon to Homey
-	socket.on("add_toon", function (device, callback) {
-
-		// Set toon to the chosen device
-		toon.setAgreement(device.data.id);
-
-		// Store device globally
-		devices.push(device);
-
-		// Store reference to toon instance
-		toons.push({id: device.data.id, toon: toon});
-	});
-
-	// Add device to homey
-	socket.on("add_device", function (device, callback) {
-
-		// Set toon to the chosen device
-		toon.setAgreement(device.data.id);
-
-		// Store device globally
-		devices.push(device);
-
-		// Store reference to toon instance
-		toons.push({id: device.data.id, toon: toon});
+	// Pairing done
+	socket.on('disconnect', () => {
+		temp_devices = [];
 	})
 };
 
@@ -158,17 +108,14 @@ module.exports.capabilities = {
 		get: function (device_data, callback) {
 			if (device_data instanceof Error) return callback(device_data);
 
-			// Get toon instance
-			var toon = getToon(device_data.id);
-			toon = (toon != null && toon.hasOwnProperty("toon")) ? toon.toon : null;
+			// Get device
+			let device = getDevice(device_data);
 
 			// Check if found
-			if (toon != null) {
+			if (device && device.client) {
 
 				// Fetch target temperature from api
-				toon.getTargetTemperature(function (err, result) {
-					callback(err, (result / 100));
-				});
+				callback(null, device.client.target_temperature);
 			}
 			else {
 
@@ -180,15 +127,14 @@ module.exports.capabilities = {
 		set: function (device_data, temperature, callback) {
 			if (device_data instanceof Error) return callback(device_data);
 
-			// Get toon instance
-			var toon = getToon(device_data.id);
-			toon = (toon != null && toon.hasOwnProperty("toon")) ? toon.toon : null;
+			// Get device
+			let device = getDevice(device_data);
 
 			// Check if found
-			if (toon != null) {
+			if (device && device.client) {
 
 				// Set temperature via api
-				toon.setTemperature(Math.round(temperature * 2) / 2, function (err, result) {
+				device.client.setTargetTemperature(Math.round(temperature * 2) / 2, function (err, result) {
 					callback(err, result);
 				});
 			}
@@ -205,17 +151,14 @@ module.exports.capabilities = {
 		get: function (device_data, callback) {
 			if (device_data instanceof Error) return callback(device_data);
 
-			// Get toon instance
-			var toon = getToon(device_data.id);
-			toon = (toon != null && toon.hasOwnProperty("toon")) ? toon.toon : null;
+			// Get device
+			let device = getDevice(device_data);
 
 			// Check if found
-			if (toon != null) {
+			if (device && device.client) {
 
 				// Fetch measured temperature
-				toon.getMeasureTemperature(function (err, result) {
-					callback(err, (result / 100));
-				});
+				callback(null, device.client.measure_temperature);
 			}
 			else {
 
@@ -235,38 +178,32 @@ function startPolling() {
 	// Poll every 20 seconds
 	setInterval(function () {
 
-		// Loop over all devices
-		for (var i = 0; i < devices.length; i++) {
-			var device = devices[i];
+		// Loop over the devices
+		devices.forEach(device => {
 
-			// Get toon object
-			var toon = getToon(device.data.id);
-			if (toon != null) {
-
-				// Get toon instance
-				toon = toon.toon;
+			if (device && device.client) {
 
 				// Check for updated target temperature
-				toon.getTargetTemperature(function (err, result) {
+				device.client.getTargetTemperature((err, result) => {
 					var updatedTemperature = result / 100;
 
 					// If updated temperature is not equal to prev temperature
-					if (updatedTemperature != device.data.target_temperature) {
+					if (device.client.target_temperature && updatedTemperature != device.client.target_temperature) {
 
 						// Do a realtime update
 						module.exports.realtime(device.data, "target_temperature", updatedTemperature);
 					}
 
 					// And store updated value
-					device.data.target_temperature = result / 100;
+					device.client.target_temperature = result / 100;
 				});
 
 				// Check for updated measured temperature
-				toon.getMeasureTemperature(function (err, result) {
+				device.client.getMeasureTemperature((err, result) => {
 					var updatedTemperature = result / 100;
 
 					// If updated temperature is not equal to prev temperature
-					if (updatedTemperature != device.data.measure_temperature) {
+					if (device.client.measure_temperature && updatedTemperature != device.data.measure_temperature) {
 
 						// Do a realtime update
 						module.exports.realtime(device.data, "measure_temperature", updatedTemperature);
@@ -276,54 +213,111 @@ function startPolling() {
 					device.data.measure_temperature = result / 100;
 				});
 			}
-		}
+		});
 	}, 15000);
 }
+
+/**
+ * Adds a new device and initializes
+ * it and its client.
+ * @param device_data
+ * @param callback
+ */
+module.exports.added = function (device_data, callback) {
+
+	// Init newly added device
+	initDevice(device_data);
+
+	// Callback success
+	callback(null, true);
+};
 
 /**
  * Delete devices internally when users removes one
  * @param device_data
  */
 module.exports.deleted = function (device_data) {
-	var device = getDevice(device_data.id);
-	var toon = getToon(device_data.id);
 
-	// Find toon and delete
-	var toon_index = toons.indexOf(toon);
-	if (toon_index > -1) {
-		toons.splice(toon_index, 1);
-	}
-
-	// Find device and delete
-	var device_index = devices.indexOf(device);
-	if (device_index > -1) {
-		devices.splice(device_index, 1);
-	}
+	// Reset array with device removed
+	devices = devices.filter(function (device) {
+		return device.data.id !== device_data.id
+	});
 };
+
+/**
+ * Initializes a device, based on device_data
+ * @param device_data
+ */
+function initDevice(device_data) {
+
+	// Create new toon instance
+	var client = new Toon(Homey.env.TOON_KEY, Homey.env.TOON_SECRET);
+
+	// Fetch stored access tokens and store them in toon object
+	if (Homey.manager('settings').get(`toon_${device_data.id}_access_token`)) client.access_token = Homey.manager('settings').get(`toon_${device_data.id}_access_token`);
+	if (Homey.manager('settings').get(`toon_${device_data.id}_refresh_token`)) client.refresh_token = Homey.manager('settings').get(`toon_${device_data.id}_refresh_token`);
+
+	// Get agreements from client
+	client.getAgreements((err, results) => {
+		if (!err && results) {
+
+			// Loop over results
+			results.forEach(function (data) {
+
+				// Check if agreementId is device id
+				if (data.agreementId === device_data.id) {
+
+					// Store newly set agreement
+					client.setAgreement(data.agreementId);
+
+					// Store constructed device
+					devices.push({
+						name: "Toon",
+						data: {
+							id: device_data.id
+						},
+						client: client
+					});
+
+					// Set available again
+					module.exports.setAvailable(device_data);
+
+					// TODO add insights logging for electicity and gas usage
+					// TODO add pushEvent instead of polling
+					// Homey.manager('insights').createLog( 'electricity_usage', {
+					// 	label: {
+					// 		en: 'Electricity Usage'
+					// 	},
+					// 	type: 'number',
+					// 	units: {
+					// 		en: 'kWh'
+					// 	},
+					// 	decimals: 2,
+					// 	chart: 'stepLine' // prefered, or default chart type. can be: line, area, stepLine, column, spline, splineArea, scatter
+					// }, function callback(err , success){
+					// 	if( err ) return Homey.error(err);
+					// 	Homey.manager('insights').createEntry( 'power_usage', 9, new Date(), function(err, success){
+					// 		if( err ) return Homey.error(err);
+					// 	})
+					// });
+				}
+			});
+		}
+	});
+}
 
 /**
  * Gets a device based on an id
- * @param device_id
+ * @param device_data
  * @returns {*}
  */
-function getDevice(device_id) {
-	for (var x = 0; x < devices.length; x++) {
-		if (devices[x].data.id === device_id) {
-			return devices[x];
-		}
-	}
-};
+function getDevice(device_data) {
 
-/**
- * Gets the toon socket instance based on device id
- * @param device_id
- * @returns {*}
- */
-function getToon(device_id) {
-	for (var x = 0; x < toons.length; x++) {
-		if (toons[x].id === device_id) {
-			return toons[x];
-		}
-	}
-};
+	// If only id provided
+	if (typeof device_data != "object") device_data = { id: device_data };
 
+	// Loop over devices
+	return devices.find(function (device) {
+		return device.data.id === device_data.id;
+	});
+}
