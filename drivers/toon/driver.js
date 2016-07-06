5e5e8972849d3765d6d0e255819e14194c6db563
Robin Bolscher <robinbolscher@gmail.com>
2016-07-06 11:28:42 +0200

    Fix setting target_temperature, general improvements

--- drivers/toon/driver.js
+++ drivers/toon/driver.js
@@ -18,8 +18,11 @@ module.exports.init = function (devices_data, callback) {
 		initDevice(device_data);
 	});
 
-	// Start listening for changes
+	// Start polling for changes
 	startPolling();
+
+	// Start listening for events
+	listenForEvents();
 
 	// Ready
 	callback(null, true);
@@ -68,7 +71,7 @@ module.exports.pair = function (socket) {
 
 									// Store device temporarily
 									temp_devices.push({
-										name: "Toon",
+										name: (results.length > 1) ? `Toon: ${data.street} ${data.houseNumber} , ${data.postalCode} ${data.city.charAt(0)}${data.city.slice(1).toLowerCase()}` : "Toon",
 										data: {
 											id: data.agreementId
 										}
@@ -84,6 +87,7 @@ module.exports.pair = function (socket) {
 			}
 		);
 	});
+
 
 	// Show list of devices
 	socket.on("list_devices", (data, callback) => {
@@ -112,10 +116,8 @@ module.exports.capabilities = {
 			let device = getDevice(device_data);
 
 			// Check if found
-			if (device && device.client) {
-
-				// Fetch target temperature from api
-				callback(null, device.client.target_temperature);
+			if (device && device.state.target_temperature) {
+				callback(null, device.state.target_temperature);
 			}
 			else {
 
@@ -135,6 +137,10 @@ module.exports.capabilities = {
 
 				// Set temperature via api
 				device.client.setTargetTemperature(Math.round(temperature * 2) / 2, function (err, result) {
+
+					// Store updated temperature internally
+					device.state.target_temperature = (Math.round(temperature * 2) / 2);
+
 					callback(err, result);
 				});
 			}
@@ -155,10 +161,8 @@ module.exports.capabilities = {
 			let device = getDevice(device_data);
 
 			// Check if found
-			if (device && device.client) {
-
-				// Fetch measured temperature
-				callback(null, device.client.measure_temperature);
+			if (device && device.state.measure_temperature) {
+				callback(null, device.state.measure_temperature);
 			}
 			else {
 
@@ -168,54 +172,6 @@ module.exports.capabilities = {
 		}
 	}
 };
-
-/**
- * Toon doesn't support realtime, therefore we have to poll
- * for changes considering the measured and target temperature
- */
-function startPolling() {
-
-	// Poll every 20 seconds
-	setInterval(function () {
-
-		// Loop over the devices
-		devices.forEach(device => {
-
-			if (device && device.client) {
-
-				// Check for updated target temperature
-				device.client.getTargetTemperature((err, result) => {
-					var updatedTemperature = result / 100;
-
-					// If updated temperature is not equal to prev temperature
-					if (device.client.target_temperature && updatedTemperature != device.client.target_temperature) {
-
-						// Do a realtime update
-						module.exports.realtime(device.data, "target_temperature", updatedTemperature);
-					}
-
-					// And store updated value
-					device.client.target_temperature = result / 100;
-				});
-
-				// Check for updated measured temperature
-				device.client.getMeasureTemperature((err, result) => {
-					var updatedTemperature = result / 100;
-
-					// If updated temperature is not equal to prev temperature
-					if (device.client.measure_temperature && updatedTemperature != device.data.measure_temperature) {
-
-						// Do a realtime update
-						module.exports.realtime(device.data, "measure_temperature", updatedTemperature);
-					}
-
-					// And store updated value
-					device.data.measure_temperature = result / 100;
-				});
-			}
-		});
-	}, 15000);
-}
 
 /**
  * Adds a new device and initializes
@@ -238,11 +194,136 @@ module.exports.added = function (device_data, callback) {
  */
 module.exports.deleted = function (device_data) {
 
+	let device = getDevice(device_data);
+	if (device && device.client) device.client.destroy();
+
 	// Reset array with device removed
 	devices = devices.filter(function (device) {
 		return device.data.id !== device_data.id
 	});
 };
+
+/**
+ * Toon doesn't support realtime, therefore we have to poll
+ * for changes considering the measured and target temperature
+ */
+function startPolling() {
+
+	setInterval(function () {
+
+		// Refresh access tokens
+		refreshAccessTokens();
+
+	}, 20 * 60 * 1000);
+
+	// Poll every 20 seconds
+	setInterval(function () {
+
+		// Start fetching and updating data
+		fetchAndUpdateData();
+
+	}, 15000);
+}
+
+/**
+ * Refreshes the access tokens for
+ * all devices.
+ */
+function refreshAccessTokens() {
+
+	// Loop over the devices
+	devices.forEach(device => {
+
+		if (device && device.client) {
+
+			// If once manually authorized
+			if (device.client.refresh_token) {
+
+				// Fetch new tokens
+				device.client.refreshAccessToken();
+			}
+		}
+	});
+}
+
+/**
+ * Start fetching data for all devices,
+ * and look for updated values.
+ */
+function fetchAndUpdateData() {
+
+	// Loop over the devices
+	devices.forEach(device => {
+
+		console.log(`Toon: fetch and update data for ${devices.length} devices`);
+
+		if (device && device.client) {
+
+			// Fetch new status
+			device.client.getStatus((err, result) => {
+
+				console.log("Toon: get status:");
+				console.log(result);
+
+				if (!err && result) {
+
+					console.log("Toon: found new data, updating");
+
+					// Format values
+					result.currentSetpoint = Math.round((result.currentSetpoint / 100) * 10) / 10;
+					result.currentTemp = Math.round((result.currentTemp / 100) * 10) / 10;
+
+					// If updated temperature is not equal to prev temperature
+					if (device.state.target_temperature && result.currentSetpoint != device.state.target_temperature) {
+
+						console.log("Toon: emit realtime target_temperature event: " + result.currentSetpoint);
+
+						// Do a realtime update
+						module.exports.realtime(device.data, "target_temperature", result.currentSetpoint);
+					}
+
+					// And store updated value
+					device.state.target_temperature = result.currentSetpoint;
+
+					// If updated temperature is not equal to prev temperature
+					if (device.state.measure_temperature && result.currentTemp != device.state.measure_temperature) {
+
+						console.log("Toon: emit realtime measure_temperature event: " + result.currentTemp);
+
+						// Do a realtime update
+						module.exports.realtime(device.data, "measure_temperature", result.currentTemp);
+					}
+
+					// And store updated value
+					device.state.measure_temperature = result.currentTemp;
+				} else {
+					console.log("Toon: no new data found");
+				}
+			});
+		}
+	});
+}
+
+/**
+ * Register webhook and listen for
+ * calls to it.
+ */
+function listenForEvents() {
+
+	// Register webhook
+	// TODO use webhook registered under info@athom.com
+	Homey.manager('cloud').registerWebhook(Homey.env.TOON_WEBHOOK_ID, Homey.env.TOON_WEBHOOK_SECRET, {}, (data) => {
+			//TODO handle incoming event with updated data store it in this
+			console.log("Incoming data")
+			console.log(data.body.updateDataSet);
+
+		},
+		(err, result) => {
+			if (err) console.log("Toon: failed to setup webhook");
+			else if (result) console.log("Toon: succes setting up webhook");
+		}
+	);
+}
 
 /**
  * Initializes a device, based on device_data
@@ -268,38 +349,51 @@ function initDevice(device_data) {
 				if (data.agreementId === device_data.id) {
 
 					// Store newly set agreement
-					client.setAgreement(data.agreementId);
+					client.setAgreement(data.agreementId, function () {
 
-					// Store constructed device
-					devices.push({
-						name: "Toon",
-						data: {
-							id: device_data.id
-						},
-						client: client
+						// Store constructed device
+						devices.push({
+							name: "Toon",
+							data: {
+								id: device_data.id
+							},
+							state: {},
+							client: client
+						});
+
+						// Set available again
+						module.exports.setAvailable(device_data);
+
+						// Start fetching and updating data
+						fetchAndUpdateData();
+
+						// Subscribe to events from toon
+						client._basePOST("pushEvent", {
+							"callbackUrl": Homey.env.TOON_WEBHOOK_URL,
+							"applicationId": Homey.env.TOON_KEY
+						}, function (err, result) {
+							if (err) console.log("pushEvent failed to register");
+							else if (result) console.log('pushEvent registered');
+						});
+
+						// TODO add insights logging for electicity and gas usage
+						// Homey.manager('insights').createLog( 'electricity_usage', {
+						// 	label: {
+						// 		en: 'Electricity Usage'
+						// 	},
+						// 	type: 'number',
+						// 	units: {
+						// 		en: 'kWh'
+						// 	},
+						// 	decimals: 2,
+						// 	chart: 'stepLine' // prefered, or default chart type. can be: line, area, stepLine, column, spline, splineArea, scatter
+						// }, function callback(err , success){
+						// 	if( err ) return Homey.error(err);
+						// 	Homey.manager('insights').createEntry( 'power_usage', 9, new Date(), function(err, success){
+						// 		if( err ) return Homey.error(err);
+						// 	})
+						// });
 					});
-
-					// Set available again
-					module.exports.setAvailable(device_data);
-
-					// TODO add insights logging for electicity and gas usage
-					// TODO add pushEvent instead of polling
-					// Homey.manager('insights').createLog( 'electricity_usage', {
-					// 	label: {
-					// 		en: 'Electricity Usage'
-					// 	},
-					// 	type: 'number',
-					// 	units: {
-					// 		en: 'kWh'
-					// 	},
-					// 	decimals: 2,
-					// 	chart: 'stepLine' // prefered, or default chart type. can be: line, area, stepLine, column, spline, splineArea, scatter
-					// }, function callback(err , success){
-					// 	if( err ) return Homey.error(err);
-					// 	Homey.manager('insights').createEntry( 'power_usage', 9, new Date(), function(err, success){
-					// 		if( err ) return Homey.error(err);
-					// 	})
-					// });
 				}
 			});
 		}
